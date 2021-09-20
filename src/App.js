bde80dd9564b5880f80a05919edc8808ba1af508
Norton <nortonsantos79@gmail.com>
2021-09-19 21:11:18 -0300

    implemented caching

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 
 const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = "100";
+
 const PATH_BASE = "https://hn.algolia.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage=";
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 // const isSearched = (searchTerm) => (item) =>
 //   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -21,9 +25,12 @@ class App extends React.Component {
     super();
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: "",
       searchTerm: DEFAULT_QUERY,
     };
+
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
 
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
 
@@ -36,12 +43,30 @@ class App extends React.Component {
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
   }
 
-  setSearchTopStories(result) {
-    this.setState({ result });
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page },
+      },
+    });
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
       .then((response) => response.json())
       .then((result) => this.setSearchTopStories(result))
       .catch((error) => error);
@@ -49,22 +74,32 @@ class App extends React.Component {
 
   componentDidMount() {
     const { searchTerm } = this.state;
-
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
     const isNotId = (item) => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
+
     this.setState({
-      result: { ...this.state.result, hits: updatedHits },
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page },
+      },
     });
   }
 
   onSearchSubmit(e) {
     e.preventDefault();
     const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
   }
 
   onSearchChange(e) {
@@ -72,7 +107,13 @@ class App extends React.Component {
   }
 
   render() {
-    const { searchTerm, result } = this.state;
+    const { searchTerm, results, searchKey } = this.state;
+
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
 
     return (
       <div className="page">
@@ -85,12 +126,14 @@ class App extends React.Component {
             Search
           </Search>
         </div>
-        {result ? (
-          <Table
-            list={result.hits}
-            onDismiss={this.onDismiss}
-          />
-        ) : null}
+        {list ? <Table list={list} onDismiss={this.onDismiss} /> : null}
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
+            More
+          </Button>
+        </div>
       </div>
     );
   }
@@ -112,8 +155,6 @@ const Search = ({ value, onChange, onSubmit, children }) => {
     </form>
   );
 };
-
-
 
 const Table = ({ list, onDismiss }) => {
   return (
